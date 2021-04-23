61144c319634a913fc699b314a45c7e7ac100ef6
lijemutu <erlowarlo@gmail.com>
2021-04-22 20:18:06 -0500

    clean data

--- src/clean.py
+++ src/clean.py
@@ -22,8 +22,8 @@ def clean_monar():
     clean_data = []
     with open('response_monar.json','r',encoding='utf-8') as responseMonar:
         jsonMonar = json.load(responseMonar)
-        for estudioCategoriaName, dict in jsonMonar.items():
-            for estudioName, estudioInfo in dict.items():
+        for estudioCategoriaName, dictMonar in jsonMonar.items():
+            for estudioName, estudioInfo in dictMonar.items():
                 study = {}
                 estudioInfo1 = estudioInfo.split('\t\t\t\t\t')
                 study['Study'] = estudioName
@@ -35,5 +35,39 @@ def clean_monar():
         with open('data_Monar.json', 'w', encoding="utf-8") as outfile:
             json.dump(clean_data, outfile, ensure_ascii=False)
 
+
+def clean_olab():
+    clean_data = []
+    with open('response_olab.json','r',encoding='utf-8') as responseOlab:
+        jsonOlab = json.load(responseOlab)
+        for estudioCategoriaName, dictOlab in jsonOlab.items():
+            for estudio in dictOlab:
+                study = {}
+                study['Study'] = estudio[0]
+                study['Description'] = ''
+                try:
+                    study['Price'] = float(re.search(r"[-+]?\d*\.\d+|\d+", estudio[1].strip()).group())
+                except:
+                    continue
+                study['Category'] = estudioCategoriaName
+                study['Study_id'] = ''
+                clean_data.append(study)
+        with open('data_Olab.json', 'w', encoding="utf-8") as outfile:
+            json.dump(clean_data, outfile, ensure_ascii=False)
+        
+def clean_salud_digna():
+    clean_data = []
+    with open('response_salud_digna.json','r',encoding='latin-1') as responseSD:
+        jsonSaludDigna = json.load(responseSD)
+        for estudios in jsonSaludDigna:
+            study = {}
+            study['Study'] = estudios['Descripcion']
+            study['Description'] = estudios['Preparacion']
+            study['Price'] = round(float(re.search(r"[-+]?\d*\.\d+|\d+", estudios['Precio'].strip()).group())*1.16)
+            study['Category'] = estudios['Estudio']
+            study['Study_id'] = ''
+            clean_data.append(study)
+        with open('data_salud_digna.json', 'w', encoding="utf-8") as outfile:
+            json.dump(clean_data, outfile, ensure_ascii=False)
 if __name__ == "__main__":
-    clean_monar()
+    clean_salud_digna()
